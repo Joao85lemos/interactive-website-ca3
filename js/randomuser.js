95bcd97f659a9c0ba88016735cd7f4520df60232
Joao <joaopaulo_lemos07@hotmail.com>
2021-05-12 15:25:37 -0300

    random user api added

--- js/randomuser.js
+++ js/randomuser.js
@@ -4,22 +4,22 @@ fetch('https://randomuser.me/api/?results=5').then(response => response.json()).
 
     userData.forEach(obj => {
         let content = `
-            <div class="profile_card col-sm">
-                <div class="card" style="width: 18rem;">
-                <img src="${obj.picture.large}" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-text">Name: ${obj.name.first} ${obj.name.last}</h5>
-                    <p class="card-text">Phone number: ${obj.cell}</p>
-                    <p class="card-text">DOB: ${obj.dob.date}</p>
-                    <p class="card-text">Age: ${obj.dob.age}</p>
-                    <p class="card-text">Email ID: ${obj.email}</p>
-                    <p class="card-text">City: ${obj.location.city}</p>
-                    <p class="card-text">Country: ${obj.location.country}</p>
-                    <p class="card-text">PostCode: ${obj.location.postcode}</p>
-                </div>
-                </div>
-            </div>
-        `;
-        document.getElementById("profile_card").innerHTML += content;
+                    <div class="card" style="width: 18rem;">
+                      <img src="${obj.picture.large}" class="card-img-top" alt="customer card image">
+                      <div class="card-body">
+                        <h5 class="card-title">${obj.name.first} ${obj.name.last}</h5>
+                      </div>
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Phone number: ${obj.cell}</li>
+                        <li class="list-group-item">DOB: ${obj.dob.date}</li>
+                        <li class="list-group-item">Age: ${obj.dob.age}</li>
+                        <li class="list-group-item">Email ID: ${obj.email}</li>
+                        <li class="list-group-item">City: ${obj.location.city}</li>
+                        <li class="list-group-item">Country: ${obj.location.country}</li>
+                        <li class="list-group-item">PostCode: ${obj.location.postcode}</li>
+                      </ul>
+                    </div>
+                    `;
+        document.getElementById("customer-card").innerHTML += content;
     });
 });
